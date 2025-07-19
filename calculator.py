b7923c9d7bcad0e67eb76f3e5fe969999c11323c
s-norgaarda <s-norgaarda@bsd405.org>
2025-07-18 18:12:12 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,15 +1,15 @@
 class Calculator:
     def multiply(factor1: float, factor2: float) -> float:
-        pass
+        return factor1 * factor2
 
     def divide(dividend: float, divisor: float) -> float:
-        pass
+        return factor1 / factor2
     
     def add(number1: float, number2: float) -> float:
-        pass
+        return factor1 + factor2
     
     def subtract(minuend: float, subtrahend: float) -> float:
-        pass
+        return factor1 - factor2
     
     def power(base: float, exponent: int) -> float:
         return base ** exponent
@@ -21,3 +21,5 @@ class Calculator:
 #assert Calculator.divide(6.6, 2.2) == 3
 #assert Calculator.add(2.2, 3.3) == 5.5
 #assert Calculator.subtract(5.5, 2.2) == 3.3
+
+# + - / *
